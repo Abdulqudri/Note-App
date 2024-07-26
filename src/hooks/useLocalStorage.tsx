74c97118337efd75e046e993ae348209bc29134a
AbdulQudri Abdullahi <abdulqudir2003@gmail.com>
2024-07-26 05:52:35 +0000

    removing unwanted code

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,4 +1,6 @@
 import {useEffect, useState} from "react";
+
+
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
 	const [value, setValue] = useState<T>(() => {
 		const jsonValue = localStorage.getItem(key);
